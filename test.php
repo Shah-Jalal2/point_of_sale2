04f05e35e7e0974b60ef6182a6ccfef4f8758897
JALAL <shah.jalal0894@gmail.com>
2020-12-19 00:39:01 +0600

    test create order front end

--- test.php
+++ test.php
@@ -27,7 +27,7 @@ include "header.php";
     <section class="content">
 
         <!-- Default box -->
-        <div class="card">
+        <div class="card card-success">
 
             <form action="" method="post" name="">
 
@@ -40,27 +40,28 @@ include "header.php";
 
             <div class="card-body">
 
-<!--                <div class="col-md-6">-->
-<!---->
-<!--                    <div class="form-group">-->
-<!--                        <label for="">Customer Name</label>-->
-<!--                        <input type="text" class="form-control" id="txtCustomer" placeholder="Enter Customer Name" required>-->
-<!--                    </div>-->
-<!---->
-<!--                </div>-->
-<!---->
-<!--                <div class="col-md-6">-->
-<!---->
-<!--                    <h2>Date</h2>-->
-<!---->
-<!--                </div>-->
-
                 <div class="row">
                     <div class="col-md-6">
+
+                        <!--=========================-->
+                        <!--    Customer Name           -->
+                        <!--=========================-->
+
                         <div class="form-group">
+
                             <label for="">Customer Name</label>
-                            <input type="text" class="form-control" id="txtCustomer" placeholder="Enter Customer Name" required>
+
+                            <div class="input-group">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                </div>
+
+                                <input type="text" class="form-control" id="txtCustomer" placeholder="Enter Customer Name" required>
+                            </div>
+
                         </div>
+
                     </div>
 
 
@@ -68,7 +69,7 @@ include "header.php";
 
                         <div class="form-group">
                             <!-- Date -->
-                            <div class="form-group">
+<!--                            <div class="form-group">-->
                                 <label>Date:</label>
                                 <div class="input-group date" id="reservationdate" data-target-input="nearest">
                                     <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
@@ -76,13 +77,11 @@ include "header.php";
                                         <div class="input-group-text"><i class="fa fa-calendar"></i></div>
                                     </div>
                                 </div>
-                            </div>
+<!--                            </div>-->
                         </div>
 
                     </div>
                 </div>
-
-
 
             </div>
 
@@ -91,15 +90,186 @@ include "header.php";
                 <!--======================================================================= -->
 
 
-                <div class="card-body"></div>
+                <div class="card-body">
+
+                    <div class="col-md-12">
+
+                        <table class="table  table-striped table-responsive-sm ">      <!-- dt resposinve is from datatable-->
+
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Search</th>
+                                <th>Stock</th>
+                                <th>Price</th>
+                                <th>Enter Quantity</th>
+                                <th>Total</th>
+                                <th>
+                                    <button type="button" name="add" class="btn btn-success btn-sm btnAdd"><span class="fa fa-plus"></span></button>
+                                </th>
+                            </tr>
+                            </thead>
+
+                        </table>
+
+                    </div>
+
+                </div>
 
                 <!--=======================================================================-->
-                <!--                     This is for table           -->
+                <!--                     This is for etc           -->
                 <!--======================================================================= -->
 
-            <div class="card-body"></div>
+            <div class="card-body">
+
+                <div class="row">
+                    <div class="col-md-6">
+
+                        <!--=========================-->
+                        <!--    Subtotal           -->
+                        <!--=========================-->
+
+                        <div class="form-group">
+                            <label for="">Subtotal</label>
+
+                            <div class="input-group">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-dollar-sign"></i></span>
+                                </div>
+
+                            <input type="text" class="form-control" id="txtSubtotal" required>
+
+                            </div>
+
+                        </div>
+
+                        <!--=========================-->
+                        <!--    Tax           -->
+                        <!--=========================-->
+
+                        <div class="form-group">
+                            <label for="">Tax (5%)</label>
+
+                            <div class="input-group">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-dollar-sign"></i></span>
+                                </div>
+
+                            <input type="number" class="form-control" id="txtText" required>
+
+                            </div>
+                        </div>
+
+                        <!--=========================-->
+                        <!--    Discount           -->
+                        <!--=========================-->
+
+                        <div class="form-group">
+                            <label for="">Discount</label>
+
+                            <div class="input-group">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-dollar-sign"></i></span>
+                                </div>
+
+                            <input type="number" class="form-control" id="txtDiscount" required>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6">
+
+                        <!--=========================-->
+                        <!--    Total           -->
+                        <!--=========================-->
+
+                        <div class="form-group">
+                            <label for="">Total</label>
+                            <div class="input-group">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-dollar-sign"></i></span>
+                                </div>
+
+                            <input type="text" class="form-control" id="txtTotal" required>
+
+                            </div>
+                        </div>
+
+                        <!--=========================-->
+                        <!--    Paid           -->
+                        <!--=========================-->
+
+                        <div class="form-group">
+                            <label for="">Paid</label>
+
+                            <div class="input-group">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-dollar-sign"></i></span>
+                                </div>
+
+                            <input type="number" class="form-control" id="txtPaid" required>
+
+                            </div>
+                        </div>
+
+                        <!--=========================-->
+                        <!--    Due           -->
+                        <!--=========================-->
+
+                        <div class="form-group">
+                            <label for="">due</label>
+
+                            <div class="input-group">
+
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-dollar-sign"></i></span>
+                                </div>
+
+                            <input type="number" class="form-control" id="txtDue" required>
+
+                            </div>
+                        </div>
+
+<!--                        <br>-->
+                        <!-- radio -->
+
+                        <label>Payement Method</label>
+                        <div class="form-group clearfix">
+
+                            <div class="icheck-danger d-inline">
+                                <input type="radio" name="r2" checked id="radioDanger1">CASH
+                                <label for="radioDanger1">
+                                </label>
+                            </div>
+                            <div class="icheck-danger d-inline">
+                                <input type="radio" name="r2" id="radioDanger2">CARD
+                                <label for="radioDanger2">
+                                </label>
+                            </div>
+                            <div class="icheck-danger d-inline">
+                                <input type="radio" name="r2" id="radioDanger3"> CHECK
+                                <label for="radioDanger3">
+                                </label>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
 
         </div>
+<!--                <hr>-->
+                <div class="form-group" align="center">
+                    <button type="submit" class="btn btn-info" name="saveOrder" value="save order">Save Order</button>
+                </div>
 
         </form>
 
